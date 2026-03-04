7393bb33de4b5b8d261d9e5bca506fc9f5681f1c
Shaheer <shaheerabdullah944@gmail.com>
2026-03-04 21:57:07 +0500

    Add cylinder volume calculation logic
    Standardized formula application: 
    V = pi r^2 h
    High-precision constant definition for pi (3.14159$)
    User-driven input handling for radius and height using float data types
    Clean CLI output formatting for improved user experience

---
+++ Volume-Calculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float Volume=0;
+	float radius=0;
+	float const pi=3.14159;
+	float height=0;
+	
+	cout<<"Enter The radius of circle:\n";
+	cin>>radius;
+	cout<<"Enter the height:"<<"in centimeters\n";
+	cin>>height;
+	
+	Volume=radius*radius*pi*height;
+	cout<<"Volume of circle is:\t\t\t"<<Volume;
+
+	
+	
+	
+	
+	
+	
+}
